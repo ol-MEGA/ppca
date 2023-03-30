fd4a3dfe4f67c37f87a5be0158e6e003466f100f
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-03-30 17:30:32 +0200

    minor

--- recipes/LibriParty/VAD/run_VAD.sh
+++ recipes/LibriParty/VAD/run_VAD.sh
@@ -8,14 +8,14 @@ commonlanguage_folder=/home/jule/datasets/common_voice_kpd
 #VAD configs
 skip_prep=True
 sample_rate=16000
-n_mels=80 #80, 40, 20, 10
+n_mels=40 #80, 40, 20, 10
 time_factor=1 #1, 4, 8, 12
 win_length=$((25*time_factor))
 hop_length=$((10*time_factor))
 n_fft=$((sample_rate*win_length/1000))
 
 #=========== end config ===========
-echo Running VAD with win_length $win_length ms, hop_length $hop_length ms, nfft $n_fft ...
+echo Running VAD with $n_mels mels, win_length $win_length ms, hop_length $hop_length ms, nfft $n_fft ...
 
 python train.py hparams/train.yaml --data_folder=$data_folder --musan_folder=$musan_folder --commonlanguage_folder=$commonlanguage_folder \
         --skip_prep=$skip_prep --n_mels=$n_mels --win_length=$win_length --hop_length=$hop_length --n_fft=$n_fft --device=cuda:1
