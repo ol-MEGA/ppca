deddf06d2c392897f34c94c4f7a11c8034c903fa
fra1993 <francesco.nespoli@hotmail.it>
2023-04-27 12:37:43 +0200

    Added cosine similarity and score normalization for ECAPA
    Speechbrain uses cosine similarity for ECAPA (not sure why not plda)

--- custom_training/ASV/testing_scripts/run_plda_verification/score_all_plda.sh
+++ custom_training/ASV/testing_scripts/run_plda_verification/score_all_plda.sh
@@ -7,6 +7,6 @@ scoring_files=$(find $veridir -type f)
 
 for verifile in $scoring_files; do
 
-   $py 2.1_speaker_verification_PLDA_VPC2022.py $verifile
+   $py speaker_verification_plda.py $verifile
 
 done
--- custom_training/ASV/testing_scripts/run_plda_verification/speaker_verification_cosine_similarity.py
+++ custom_training/ASV/testing_scripts/run_plda_verification/speaker_verification_cosine_similarity.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Recipe for training a speaker verification system based on PLDA using any dataset.
 The system employs a pre-trained model followed by a PLDA transformation.
@@ -51,6 +50,7 @@ def compute_embeddings(wavs, wav_lens):
         feats = params["mean_var_norm"](feats, wav_lens)
         embeddings = params["embedding_model"](feats, wav_lens)
     return embeddings.squeeze(1)
+
 
 def emb_computation_loop(split, set_loader, stat_file):
     """Computes the embeddings and saves the in a stat file"""
@@ -106,13 +106,13 @@ def emb_computation_loop(split, set_loader, stat_file):
         embeddings = numpy.empty(shape=[0, params["emb_dim"]], dtype=numpy.float64)
         spks = numpy.unique(numpy.array([i.split(params["separator"])[0] for i in stat_obj.modelset]))
         for spk in spks:
-            xv_spk = numpy.zeros(shape=(1,params["emb_dim"]), dtype=float)
+            xv_spk = numpy.zeros(shape=(1, params["emb_dim"]), dtype=float)
             N = 0
-            for idx,utt in enumerate(stat_obj.modelset):
+            for idx, utt in enumerate(stat_obj.modelset):
                 if spk == utt.split(params["separator"])[0]:
                     xv_spk = xv_spk + stat_obj.stat1[idx]
-                    N +=1
-            embeddings = numpy.concatenate((embeddings, xv_spk/N), axis=0)
+                    N += 1
+            embeddings = numpy.concatenate((embeddings, xv_spk / N), axis=0)
 
         modelset = numpy.array(spks, dtype="|O")
         segset = numpy.array(spks, dtype="|O")
@@ -135,64 +135,104 @@ def emb_computation_loop(split, set_loader, stat_file):
 
     return stat_obj
 
-################################################################################################# Verification Functions
-def verification_performance_mean_spkemb(scores_plda):
-    """Computes the Equal Error Rate give the PLDA scores"""
 
-    # Create ids, labels, and scoring list for EER evaluation
-    ids = []
-    labels = []
+################################################################################################# Verification Functions
+def get_verification_scores(veri_test, train_obj, enrol_obj, test_obj):
+    """Computes positive and negative scores given the verification split."""
+    scores = []
     positive_scores = []
     negative_scores = []
-    for line in open(veri_file_path):
 
-        lab = line.split(" ")[0].rstrip().split(".")[0].strip() # speaker from the enrollmet data
-        enrol_id = line.split(" ")[1].rstrip().split(".")[0].strip() # test_id
-        test_id = line.split(" ")[2].rstrip().split(".")[0].strip() # target/non_target
-#        print(lab)
-#        print(enrol_id)
-#        print(test_id)
-#        print(scores_plda.segset)
+    save_file = os.path.join(params["output_folder"], "scores.txt")
+    s_file = open(save_file, "w")
 
-        # Find the column ID for the test utterance
-        trial_index = int(numpy.where(scores_plda.segset == enrol_id)[0][0])
-#        print(trial_index)
+    # Cosine similarity initialization
+    similarity = torch.nn.CosineSimilarity(dim=-1, eps=1e-6)
 
-        # Find row IDs for all the utterances of the enrollment speaker
-        enroll_idxs = []
-#        print(scores_plda.modelset)
-        
-        for idx,elem in enumerate(scores_plda.modelset):
-            if elem.split(params["separator"])[0] == lab:
-                    enroll_idxs.append(idx)
-#        print(elem)
-#        print(elem.split(params["separator"]))
-#        print(enroll_idxs)
+    # creating cohort for score normalization
+    if "score_norm" in params:
+        train_cohort = torch.from_numpy(numpy.stack((train_obj.stat1)))
+        train_cohort = train_cohort.reshape(train_cohort.shape[0], 1, train_cohort.shape[1])
 
-        # # Assuming enrol_id and test_id are unique
-        # i = int(numpy.where(scores_plda.modelset == enrol_id)[0][0])
-        # j = int(numpy.where(scores_plda.segset == test_id)[0][0])
-        s = []
-        for j in enroll_idxs:
-            s.append(float(scores_plda.scoremat[j,trial_index]))
-        labels.append(test_id)
-        ids.append(enrol_id + "<>" + lab)
-        if test_id == "target":
-            for S in s:
-                positive_scores.append(S)
+    for i, line in enumerate(veri_test):
+
+        # Reading verification file (enrol_file test_file label)
+        enrol_id = int(line.split(" ")[0].rstrip().split(".")[0].strip())
+        test_id = line.split(" ")[1].rstrip().split(".")[0].strip()
+        lab_pair = line.split(" ")[2].rstrip().split(".")[0].strip()
+        if lab_pair == "target":
+            lab_pair = 1
+        elif lab_pair == "nontarget":
+            lab_pair = 0
         else:
-            for S in s:
-                negative_scores.append(S)
+            print("Error with lab_pair in get_verification_scores function")
+            exit()
 
-    # Clean variable
-    del scores_plda
+        # Select enrol and test embeddings
+        enrol_idx = numpy.where(enrol_obj.modelset == str(enrol_id))
+        enrol = torch.from_numpy(enrol_obj.stat1[enrol_idx])
+
+        test_idx = numpy.where(test_obj.modelset == str(test_id))
+        test = torch.from_numpy(test_obj.stat1[test_idx])
+
+        if "score_norm" in params:
+            # Getting norm stats for enrol impostors
+            enrol_rep = enrol.repeat(train_cohort.shape[0], 1, 1)
+            score_e_c = similarity(enrol_rep, train_cohort)
+
+            if "cohort_size" in params:
+                score_e_c = torch.topk(
+                    score_e_c, k=params["cohort_size"], dim=0
+                )[0]
+
+            mean_e_c = torch.mean(score_e_c, dim=0)
+            std_e_c = torch.std(score_e_c, dim=0)
+
+            # Getting norm stats for test impostors
+            test_rep = test.repeat(train_cohort.shape[0], 1, 1)
+            score_t_c = similarity(test_rep, train_cohort)
+
+            if "cohort_size" in params:
+                score_t_c = torch.topk(
+                    score_t_c, k=params["cohort_size"], dim=0
+                )[0]
+
+            mean_t_c = torch.mean(score_t_c, dim=0)
+            std_t_c = torch.std(score_t_c, dim=0)
+
+        # Compute the score for the given sentence
+        score = similarity(enrol, test)[0]
+
+        # Perform score normalization
+        if "score_norm" in params:
+            if params["score_norm"] == "z-norm":
+                score = (score - mean_e_c) / std_e_c
+            elif params["score_norm"] == "t-norm":
+                score = (score - mean_t_c) / std_t_c
+            elif params["score_norm"] == "s-norm":
+                score_e = (score - mean_e_c) / std_e_c
+                score_t = (score - mean_t_c) / std_t_c
+                score = 0.5 * (score_e + score_t)
+
+        # write score file
+        s_file.write("%s %s %i %f\n" % (enrol_id, test_id, lab_pair, score))
+        scores.append(score)
+
+        if lab_pair == 1:
+            positive_scores.append(score)
+        else:
+            negative_scores.append(score)
+
+    s_file.close()
+    return positive_scores, negative_scores
 
     # Final EER computation
-#    print(len(positive_scores), len(negative_scores)) # both empty!!!
+    #    print(len(positive_scores), len(negative_scores)) # both empty!!!
     eer, th = EER(torch.tensor(positive_scores), torch.tensor(negative_scores))
     min_dcf, th = minDCF(torch.tensor(positive_scores), torch.tensor(negative_scores))
 
-    return eer , min_dcf
+    return eer, min_dcf
+
 
 ############################################################################################################## Data Prep
 def get_utt_ids_for_test(ids, data_dict):
@@ -202,7 +242,7 @@ def get_utt_ids_for_test(ids, data_dict):
     return mod, seg
 
 
-def dataio_prep(params): # TODO === Need to change the data IO function ===
+def dataio_prep(params):  # TODO === Need to change the data IO function ===
     "Creates the dataloaders and their data processing pipelines."
 
     data_folder_tr = params["data_folder_tr"]
@@ -212,9 +252,11 @@ def dataio_prep(params): # TODO === Need to change the data IO function ===
     train_data = sb.dataio.dataset.DynamicItemDataset.from_json(
         json_path=params["train_data"], replacements={"data_root": data_folder_tr},
     )
-    # train_data = train_data.filtered_sorted(
-    #     sort_key="duration", select_n=params["n_train_snts"]
-    # )
+
+    if params["score_norm"]:
+        train_data = train_data.filtered_sorted(
+            sort_key="length", select_n=params["n_train_snts"]
+        )
 
     # Enrol data
     enrol_data = sb.dataio.dataset.DynamicItemDataset.from_json(
@@ -231,13 +273,13 @@ def dataio_prep(params): # TODO === Need to change the data IO function ===
     datasets = [train_data, enrol_data, test_data]
 
     # 2. Define audio pipeline:
-    @sb.utils.data_pipeline.takes("wav")#, "start", "stop")
+    @sb.utils.data_pipeline.takes("wav")  # , "start", "stop")
     @sb.utils.data_pipeline.provides("sig")
-    def audio_pipeline(wav): #, start, stop):
-        #start = int(start)
-        #stop = int(stop)
-        #num_frames = stop - start
-        sig, fs = torchaudio.load(wav) #, num_frames=num_frames, frame_offset=start)
+    def audio_pipeline(wav):  # , start, stop):
+        # start = int(start)
+        # stop = int(stop)
+        # num_frames = stop - start
+        sig, fs = torchaudio.load(wav)  # , num_frames=num_frames, frame_offset=start)
         sig = sig.transpose(0, 1).squeeze(1)
         return sig
 
@@ -260,10 +302,6 @@ def dataio_prep(params): # TODO === Need to change the data IO function ===
     return train_dataloader, enrol_dataloader, test_dataloader
 
 
-
-
-
-
 ##################################################### MAIN ############################################################
 
 if __name__ == "__main__":
@@ -280,7 +318,6 @@ if __name__ == "__main__":
 
     # This file has to be created before: it basically pairs enrolment and trial utterances for PLDA scoring
     veri_file_path = params["verification_file"]
-    #download_file(params["verification_file"], veri_file_path)
 
     # Create experiment directory
     sb.core.create_experiment_directory(
@@ -301,7 +338,7 @@ if __name__ == "__main__":
 
     # Embedding file for train data
     xv_file = os.path.join(params["save_folder"], "my_training.pkl")
-    if not(os.path.isdir(params["save_folder"])):
+    if not (os.path.isdir(params["save_folder"])):
         os.mkdir(params["save_folder"])
 
     # We download the pretrained LM from HuggingFace (or elsewhere depending on
@@ -363,11 +400,6 @@ if __name__ == "__main__":
         with open(xv_file, "rb") as input:
             embeddings_stat = pickle.load(input)
 
-    # Training Gaussian PLDA model
-    logger.info("Training PLDA model")
-    params["compute_plda"].plda(embeddings_stat)
-    logger.info("PLDA training completed")
-
     # Set paths for enrol/test embeddings
     enrol_stat_file = os.path.join(params["save_folder"], "stat_enrol.pkl")
     test_stat_file = os.path.join(params["save_folder"], "stat_test.pkl")
@@ -376,7 +408,6 @@ if __name__ == "__main__":
     # Compute enrol and Test embeddings
     enrol_obj = emb_computation_loop("enrol", enrol_dataloader, enrol_stat_file)
     test_obj = emb_computation_loop("test", test_dataloader, test_stat_file)
-
 
     # Prepare Ndx Object
     if not os.path.isfile(ndx_file):
@@ -393,28 +424,22 @@ if __name__ == "__main__":
         with open(ndx_file, "rb") as input:
             ndx_obj = pickle.load(input)
 
-    # PLDA scoring
-    logger.info("PLDA scoring...")
-    scores_plda = fast_PLDA_scoring(
-        enrol_obj,
-        test_obj,
-        ndx_obj,
-        params["compute_plda"].mean,
-        params["compute_plda"].F,
-        params["compute_plda"].Sigma,
-    )
+    with open(veri_file_path) as f:
+        veri_test = [line.rstrip() for line in f]
 
-    logger.info("Computing EER... ")
+    positive_scores, negative_scores = get_verification_scores(veri_test, train_obj=embeddings_stat, enrol_obj=enrol_obj, test_obj=test_obj)
 
-    # Cleaning variable
+    # Cleaning
     del enrol_dataloader
     del test_dataloader
     del enrol_obj
     del test_obj
     del embeddings_stat
 
-    # Final EER computation
-    # eer, min_dcf = verification_performance(scores_plda, single_random_comparison=
-    eer, min_dcf = verification_performance_mean_spkemb(scores_plda)
+    eer, th = EER(torch.tensor(positive_scores), torch.tensor(negative_scores))
     logger.info("EER(%%)=%f", eer * 100)
-    logger.info("min_dcf=%f", min_dcf * 100)
+
+    min_dcf, th = minDCF(
+        torch.tensor(positive_scores), torch.tensor(negative_scores)
+    )
+    logger.info("minDCF=%f", min_dcf * 100)
