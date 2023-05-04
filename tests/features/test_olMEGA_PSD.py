f9b82bed4b3d63667aa4e86fed602fbfe5d14d6c
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-05-04 10:32:15 +0200

    test with impulse

--- tests/features/test_olMEGA_PSD.py
+++ tests/features/test_olMEGA_PSD.py
@@ -3,11 +3,13 @@ import math
 from speechbrain.dataio.dataio import read_audio
 from speechbrain.processing import features
 import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-signal = read_audio('tests/samples/single-mic/example1.wav')
-signal = signal.unsqueeze(0)
-print(signal.shape)
+#signal = read_audio('tests/samples/single-mic/example1.wav')
+#signal = signal.unsqueeze(0)
 sample_rate = 16000
+signal = torch.zeros([1, sample_rate])
+signal[0, 0] = 1
 
 compute_STFT = features.STFT(
          sample_rate=sample_rate, win_length=25, hop_length=12.5, n_fft=512
@@ -33,13 +35,25 @@ im = ax[1].imshow(10*torch.log10(psd.squeeze().t()), extent=[0, psd.shape[1], 0,
 ax[1].set_xlabel("time frame")
 ax[1].set_ylabel("freq in Hz")
 ax[1].set_aspect('auto')
-im.set_clim(-80, 0)
+ax[1].set_aspect('auto')
+divider = make_axes_locatable(ax[1])
+cax = divider.append_axes("right", size="5%", pad=0.05)
+cbar = plt.colorbar(im, cax=cax)
+cbar.set_label('PSD in dB')
+im.set_clim(-50, 0)
 
 im = ax[2].imshow(10*torch.log10(psd_smoothed.squeeze().t()), extent=[0, psd_smoothed.shape[1], 0, sample_rate/2], origin='lower', interpolation='nearest')
 ax[2].set_xlabel("time frame")
 ax[2].set_ylabel("freq in Hz")
 ax[2].set_aspect('auto')
-im.set_clim(-80, 0)
+divider = make_axes_locatable(ax[2])
+cax = divider.append_axes("right", size="5%", pad=0.05)
+cbar = plt.colorbar(im, cax=cax)
+cbar.set_label('PSD smoothed in dB')
+im.set_clim(-50, 0)
+ax2 = ax[2].twinx()
+ax2.plot(psd_smoothed.squeeze()[:, 0].t(), 'k')
 
 plt.tight_layout()
+plt.savefig('test_impulse_olMEGA.png')
 plt.show()
