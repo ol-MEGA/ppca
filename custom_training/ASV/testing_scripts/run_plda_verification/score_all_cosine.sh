b229b683bb5989be6f82f392fbf096811d1180fc
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-07-11 13:03:51 +0000

    add libri_dev

--- custom_training/ASV/testing_scripts/run_plda_verification/score_all_cosine.sh
+++ custom_training/ASV/testing_scripts/run_plda_verification/score_all_cosine.sh
@@ -4,7 +4,8 @@ data_root=$1
 json_folder=$2
 py=$3
 
-for dset in vctk_dev_{trials_f,trials_m,trials_f_common,trials_m_common} \
+for dset in libri_dev_{trials_f,trials_m} \
+            vctk_dev_{trials_f,trials_m,trials_f_common,trials_m_common} \
             libri_test_{trials_f,trials_m} \
             vctk_test_{trials_f,trials_m,trials_f_common,trials_m_common}; do
 
