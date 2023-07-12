f89e496a0a3250eb9063bb1a7cfc9fecee753408
Jule Pohlhausen <pohlhausenjule@gmail.com>
2023-07-12 11:32:51 +0200

    defined new for loop

--- custom_training/ASV/testing_scripts/run_plda_verification/score_all_plda.sh
+++ custom_training/ASV/testing_scripts/run_plda_verification/score_all_plda.sh
@@ -1,12 +1,26 @@
 #!/bin/bash
 
-veridir=$1
-py=$2
+data_root=$1
+json_folder=$2
+py=$3
 
-scoring_files=$(find $veridir -type f)
+for dset in libri_dev_{trials_f,trials_m} \
+            vctk_dev_{trials_f,trials_m,trials_f_common,trials_m_common} \
+            libri_test_{trials_f,trials_m} \
+            vctk_test_{trials_f,trials_m,trials_f_common,trials_m_common}; do
 
-for verifile in $scoring_files; do
-
-   $py speaker_verification_plda.py $verifile
+   # Get basedir name for enrollment data
+   dataset=${dset%_trials*} 
+   
+   # Define Separator
+   if [[ ${dset%%_*} == "vctk"  ]];then
+      sep="_"
+   else
+      sep="'-'"
+   fi
+   
+   $py speaker_verification_plda.py hparams/verification_ecapa.yaml \
+   --data_folder_tr=$data_root --data_folder_test=$data_root --json_folder=$json_folder \
+   --subset=$dset --subset_enrol=${dataset}_enrolls --separator=$sep 
 
 done
