46b7410bb0991df72116b34c3d8c9b4c1c464a85
Jule Pohlhausen <jule.pohlhausen@jade-hs.de>
2024-08-13 10:44:05 +0200

    add pre-emphasis, random options

--- mcadams/anonymise_dir_mcadams.py
+++ mcadams/anonymise_dir_mcadams.py
@@ -13,10 +13,13 @@ import soundfile
 import argparse
 from tqdm import tqdm
 
-def anonym(file, output_file, winLengthinms=20, shiftLengthinms=10, lp_order=20, mcadams=0.8):    
+def anonym(file, output_file, winLengthinms=20, shiftLengthinms=10, lp_order=20, mcadams=0.8, emph_coef=0.97):    
     sig, fs = librosa.load(file,sr=None)    
     eps = np.finfo(np.float32).eps
     sig = sig+eps
+
+    # pre-emphasize signal
+    sig = librosa.effects.preemphasis(sig, coef=emph_coef)
     
     # simulation parameters
     winlen = np.floor(winLengthinms*0.001*fs).astype(int)
@@ -52,7 +55,7 @@ def anonym(file, output_file, winLengthinms=20, shiftLengthinms=10, lp_order=20,
         # the choice of this value is strongly linked to the number of lpc coefficients
         # a bigger lpc coefficients number constraints the effect of the coefficient to very small variations
         # a smaller lpc coefficients number allows for a bigger flexibility
-        new_angles = np.angle(poles[ind_imag_con])**mcadams
+        new_angles = np.angle(poles[ind_imag_con]) ** mcadams
 
         # make sure new angles stay between 0 and pi
         new_angles[np.where(new_angles>=np.pi)] = np.pi        
@@ -77,6 +80,8 @@ def anonym(file, output_file, winLengthinms=20, shiftLengthinms=10, lp_order=20,
         outindex = np.arange(m*shift,m*shift+len(frame_rec))
         # overlap add
         sig_rec[outindex] += frame_rec
+    # de-emphasize signal
+    sig_rec = librosa.effects.deemphasis(sig_rec, coef=emph_coef)
     sig_rec = sig_rec/np.max(np.abs(sig_rec))
     soundfile.write(output_file, np.float32(sig_rec), fs) 
     return []
@@ -84,13 +89,15 @@ def anonym(file, output_file, winLengthinms=20, shiftLengthinms=10, lp_order=20,
 if __name__ == "__main__":
     #Parse args    
     parser = argparse.ArgumentParser()
-    parser.add_argument('--data_dir',type=str)
+    parser.add_argument('--data_dir',type=str)    
+    parser.add_argument('--dataset',type=str,default='')
+    parser.add_argument('--wav_scp',type=str,default=None)
     parser.add_argument('--anon_suffix',type=str,default='_anon')
     parser.add_argument('--n_coeffs',type=int,default=20)
     parser.add_argument('--mc_coeff',type=float,default=0.8)
     parser.add_argument('--mc_coeff_min', type=float, default=0.5)
     parser.add_argument('--mc_coeff_max', type=float, default=0.9)
-    parser.add_argument('--mc_rand', action=argparse.BooleanOptionalAction)
+    parser.add_argument('--mode', type=str,default='rand')
     parser.add_argument('--winLengthinms',type=int,default=20)
     parser.add_argument('--shiftLengthinms',type=int,default=10)
     config = parser.parse_args()
@@ -98,25 +105,49 @@ if __name__ == "__main__":
     np.random.seed(1234)
     
     #Load protocol file
-    list_name = config.data_dir + '/wav.scp'
+    if config.wav_scp is None:
+        list_name = os.path.join(config.data_dir, 'wav.scp')
+    else:
+        list_name = config.wav_scp
     list_files = np.genfromtxt(list_name,dtype='U')
     
-    output_dir = config.data_dir+config.anon_suffix
-    
-    if config.mc_rand:
+    output_dir = config.data_dir.replace(config.dataset, config.dataset+config.anon_suffix)
+    os.makedirs(output_dir, exist_ok=True)
+
+    if "rand" in config.mode:
         f = open(output_dir + '/mcadams_rand.txt', 'w')
+    
+    if "per_speaker" in config.mode or "per_rec" in config.mode:
+        old_id = ""
 
     for idx,file in enumerate(tqdm(list_files)):   
+        if "VPC" in config.wav_scp:
+            input_file = os.path.join(config.data_dir, file[1].replace('data/', ''))
+        else:
+            input_file = file[2]
+
         # change outpt dirname and create folders
-        output_file = file[2].replace(config.data_dir, output_dir)
-        os.makedirs(os.path.split(output_file)[0], exist_ok=True)
+        output_file = input_file.replace(config.data_dir, output_dir)
+        os.makedirs(os.path.dirname(output_file), exist_ok=True)
 
-        if config.mc_rand:
-            config.mc_coeff = np.random.uniform(config.mc_coeff_min, config.mc_coeff_max)
-
-            f.write(output_file + ', ' + str(config.mc_coeff) + '\n')
-
-        anonym(file[2], output_file, winLengthinms=config.winLengthinms, shiftLengthinms=config.shiftLengthinms, lp_order=config.n_coeffs, mcadams=config.mc_coeff)
+        if not os.path.exists(output_file):
+            if config.mode == "rand":
+                config.mc_coeff = np.random.uniform(config.mc_coeff_min, config.mc_coeff_max)
+                f.write(output_file + ', ' + str(config.mc_coeff) + '\n')
+            elif config.mode == "rand_per_speaker":
+                spk_id = output_file.split("/")[-1].split(".")[0].split("-")[0].split("_")[0]
+                if not spk_id == old_id:
+                    config.mc_coeff = np.random.uniform(config.mc_coeff_min, config.mc_coeff_max)
+                old_id = spk_id
+                f.write(output_file + ', ' + str(config.mc_coeff) + '\n')
+            elif config.mode == "rand_per_rec_session":
+                rec_id = "-".join(output_file.split("/")[-1].split(".")[0].split("-")[:2])
+                if not rec_id == old_id:
+                    config.mc_coeff = np.random.uniform(config.mc_coeff_min, config.mc_coeff_max)
+                old_id = rec_id
+                f.write(output_file + ', ' + str(config.mc_coeff) + '\n')
+            
+            anonym(input_file, output_file, winLengthinms=config.winLengthinms, shiftLengthinms=config.shiftLengthinms, lp_order=config.n_coeffs, mcadams=config.mc_coeff, mode=config.mode)
        
-    if config.mc_rand:
+    if "rand" in config.mode:
         f.close()
